c53ebdd5f762e221a31c4de2b6e3620f552258ac
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-14 14:17:35 -0300

    Feito até a aula 26

--- 2-react-webpack/src/app.js
+++ 2-react-webpack/src/app.js
@@ -1,8 +1,18 @@
 'use strict'
-import React from 'react';
-const App = React.createClass({
-	render: function(){
-		return <div>Aplicação</div>
+import React, {Component} from 'react';
+import Square from './square';
+import Title from './title';
+
+class App extends Component{
+	render(){
+		return(
+			<div className="container">
+				{['blue', 'red', 'green'].map((square, index) => (
+					<Square key={index} color={square} />
+				))}
+			</div>
+		)
 	}
-})
+}
+
 export default App;
