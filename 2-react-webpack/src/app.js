97b8c3a71e7e12e650d4637da62e9327a253e80f
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-13 22:46:33 -0300

    Initial commit

---
+++ 2-react-webpack/src/app.js
@@ -0,0 +1,8 @@
+'use strict'
+var React = require('react');
+var Title = React.createClass({
+	render: function(){
+		return React.createElement('h1', null, 'titulo');
+	}
+})
+module.exports = Title;
---
+++ 2-react-webpack/src/index.js
@@ -0,0 +1,8 @@
+'use strict'
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Title = require('./app');
+ReactDOM.render(
+	React.createElement(Title),
+	document.querySelector('[data-js="app"]')
+)
