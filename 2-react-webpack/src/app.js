f2f2c5207521ee7c9afa69d4c99c20df686e047f
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-14 17:47:53 -0300

    aula 32 - state

--- 2-react-webpack/src/app.js
+++ 2-react-webpack/src/app.js
@@ -1,17 +1,26 @@
 'use strict'
 import React, {Component} from 'react';
+import Button from './button';
+import Square from './square';
 
 class App extends Component{
 	constructor() {
 		super();
 		this.state = {
-			text: 'gabriel'
+			color: 'green'
 		}
 	}
 	render(){
 		return(
-			<div className="container" onClick={() => this.setState({text: 'outro texto'})}>
-				{this.state.text}
+			<div>
+				<Square color={this.state.color} />
+				{['red', 'green', 'blue'].map((color) => (
+					<Button
+						key={color}
+						handleClick={() => this.setState({color})}>
+							{color}
+					</Button>
+				))}
 			</div>
 		)
 	}
