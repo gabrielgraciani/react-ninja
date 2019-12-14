366a36cb3b031f367c9869535f8d0640444a03bb
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-14 17:18:06 -0300

    aula 28 - children

--- 2-react-webpack/src/app.js
+++ 2-react-webpack/src/app.js
@@ -1,20 +1,12 @@
 'use strict'
 import React, {Component} from 'react';
-import Square from './square';
-import Title from './title';
+import Button from './button';
 
 class App extends Component{
 	render(){
 		return(
-			/*<div className="container">
-				{['blue', 'red', 'green'].map((square, index) => (
-					<Square key={index} color={square} />
-				))}
-			</div>*/
-			<div className="container" onClick={(e) => {
-				alert('clicou');
-			}}>
-				<Square />
+			<div className="container">
+				<Button>Texto</Button>
 			</div>
 		)
 	}
