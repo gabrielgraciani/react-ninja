08ea3a7471f10f17b3ef958522722fe82ef53ce8
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-15 18:35:40 -0300

    Aula 61 - colocando spread operador app github

--- 1-introducao/github-app/src/app.js
+++ 1-introducao/github-app/src/app.js
@@ -68,13 +68,10 @@ class App extends Component{
 	render(){
 		return(
 			<AppContent
-				userinfo={this.state.userinfo}
-				repos={this.state.repos}
-				starred={this.state.starred}
+				{...this.state}
 				handleSearch={this.handleSearch}
 				getRepos={this.getRepos('repos')}
 				getStarred={this.getRepos('starred')}
-				isFetching={this.state.isFetching}
 			/>
 		)
 	}
