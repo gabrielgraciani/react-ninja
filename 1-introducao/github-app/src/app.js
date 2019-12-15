85e51a6ae00f65135e497e1a9fe65f5f51ce81fa
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-15 14:50:14 -0300

    Aula 56 - criacao da função que busca a url app github

--- 1-introducao/github-app/src/app.js
+++ 1-introducao/github-app/src/app.js
@@ -13,12 +13,18 @@ class App extends Component{
 		}
 	}
 
+	getGitHubApiUrl(username, type){
+		const internalUser = username ? `/${username}` : '';
+		const internalType = type ? `/${type}` : '';
+		return `https://api.github.com/users${internalUser}${internalType}`
+	}
+
 	handleSearch (e) {
 		const value = e.target.value;
 		const keyCode = e.which || e.keyCode;
 		const ENTER = 13;
 		if(keyCode === ENTER){
-			ajax().get(`https://api.github.com/users/${value}`)
+			ajax().get(this.getGitHubApiUrl(value))
 			.then((result) => {
 				this.setState({
 					userinfo: {
@@ -29,9 +35,8 @@ class App extends Component{
 						followers: result.followers,
 						following: result.following
 					},
-					repos: [{
-
-					}]
+					repos: [],
+					starred: []
 				})
 			})
 		}
@@ -39,7 +44,8 @@ class App extends Component{
 
 	getRepos(type){
 		return (e) =>{
-			ajax().get(`https://api.github.com/users/fdaciuk/${type}`)
+			const username = this.state.userinfo.login;
+			ajax().get(this.getGitHubApiUrl(username, type))
 			.then((result) => {
 				this.setState({
 					[type]: result.map((repo) => ({
