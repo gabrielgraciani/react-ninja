c1931c3d60cf5ac4b59f761a1433c865edc02e25
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-14 22:38:00 -0300

    Aula 43 - form textarea

--- 2-react-webpack/src/app.js
+++ 2-react-webpack/src/app.js
@@ -5,21 +5,13 @@ class App extends Component{
 	constructor(){
 		super()
 		this.state = {
-			value: '2'
 		}
 	}
 	render(){
 		return(
 			<div>
 				<form>
-					<select value={this.state.value} onChange={(e) => {
-						this.setState({value: e.target.value})
-					}}>
-						<option value="1">Opcao 1</option>
-						<option value="2">Opcao 2</option>
-						<option value="3">Opcao 3</option>
-					</select>
-
+					<textarea defaultValue="qeqeqweqweqw" />
 				</form>
 			</div>
 		)
