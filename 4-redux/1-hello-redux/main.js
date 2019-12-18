b8531b6b925ae1acd9eb1289b59fbba74ebe6344
Gabriel <luciano.rodrigues@aptasd.com.br>
2019-12-18 14:15:34 -0300

    Começo capitulo redux - aula 215

--- 4-redux/1-hello-redux/main.js
+++ 4-redux/1-hello-redux/main.js
@@ -1,3 +1,32 @@
+const counter = (state = 0, action) => {
+	switch(action.type){
+		case 'INCREMENT': return state + 1;
+		case 'DECREMENT': return state - 1
+	}
+
+	return state;
+};
+
 console.assert(
-	1 === 2
+	counter(0, {type: 'INCREMENT'})
+);
+
+console.assert(
+	counter(1, {type: 'INCREMENT'}) === 2
+);
+
+console.assert(
+	counter(2, {type: 'DECREMENT'}) === 1
+);
+
+console.assert(
+	counter(5, {type: 'DECREMENT'}) === 4
+);
+
+console.assert(
+	counter(3, {type: 'SOMEGHINT'}) === 3
+);
+
+console.assert(
+	counter(undefined, {}) === 0
 );
