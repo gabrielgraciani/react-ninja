4d931b16b0676cd3d518a262b0a207686b038043
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-20 17:53:56 -0300

    Aula 229 - Adicionando mais contadores - componente

--- 4-redux/05-react-counter/src/index.js
+++ 4-redux/05-react-counter/src/index.js
@@ -5,16 +5,9 @@ import {Provider} from 'react-redux'
 import {createStore} from 'redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducer from './redux-flow/reducers/counters'
 
-const counter = (state = 0, action) => {
-	switch (action.type) {
-		case 'INCREMENT': return state + 1;
-		case 'DECREMENT': return state - 1;
-	}
-	return state
-};
-
-const store = createStore(counter);
+const store = createStore(reducer);
 
 ReactDOM.render(
 		<Provider store={store}>
