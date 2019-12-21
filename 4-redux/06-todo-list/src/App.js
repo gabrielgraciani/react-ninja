6e96aa34bc588095cfcdfd01fb720e707d8b1b9e
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-21 14:06:37 -0300

    Aula 235 - app todo list - implementação inicial

--- 4-redux/06-todo-list/src/App.js
+++ 4-redux/06-todo-list/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-		oi
-    </div>
-  );
-}
+const App = () => (
+	<div>
+		<input type="text" />
+
+		<ul>
+			<li style={{textDecoration: 'line-through'}}>Item 1</li>
+			<li>Item 1</li>
+			<li>Item 1</li>
+			<li>Item 1</li>
+			<li>Item 1</li>
+		</ul>
+
+		<div>
+			<h3>Mostrar</h3>
+			<a href="">Exibir Todos</a> | <a href="">Finalizados</a> | <a href="">A Fazer</a>
+		</div>
+	</div>
+);
 
 export default App;
