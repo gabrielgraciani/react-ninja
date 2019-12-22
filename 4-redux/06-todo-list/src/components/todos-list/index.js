22e0c6476d34ec004d2e74c60f2b3cc511eea4f5
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-22 17:53:09 -0300

    Aula 254 - app todo list - mostrar todos filtrados

--- 4-redux/06-todo-list/src/components/todos-list/index.js
+++ 4-redux/06-todo-list/src/components/todos-list/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {toggleTodo} from '../../redux-flow/reducers/todos/action-creators'
+import {toggleTodo} from '../../redux-flow/reducers/todos/action-creators';
+import * as filterActions from '../../redux-flow/reducers/visibility-filter/actions';
 
 
-const TodosList = ({todos, handleToggleTodo}) => (
+const TodosList = ({todos, handleToggleTodo, activeFilter}) => (
 	<ul>
-		{todos.map((todo) => (
+		{getVisibleTodos(todos, activeFilter).map((todo) => (
 			<li key={todo.id}
 				style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
 				onClick={handleToggleTodo(todo.id)}>
@@ -15,14 +16,25 @@ const TodosList = ({todos, handleToggleTodo}) => (
 	</ul>
 )
 
+const getVisibleTodos = (todos, activeFilter) => {
+	const filterItems = {
+		[filterActions.SHOW_ALL]: todos,
+		[filterActions.SHOW_COMPLETED]: todos.filter((todo) => todo.completed),
+		[filterActions.SHOW_ACTIVE]: todos.filter((todo) => !todo.completed)
+	};
+
+	return filterItems[activeFilter];
+};
+
 const mapStateToProps = (state) => ({
-	todos: state.todos
-})
+	todos: state.todos,
+	activeFilter: state.visibilityFilter
+});
 
 const mapDispatchToProps = (dispatch) => ({
 	handleToggleTodo: (id) => (e) => {
 		dispatch(toggleTodo(id))
 	}
-})
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodosList)
