d3261522c10a10d269a0774f4478bd07dd5a8b3b
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-22 18:13:12 -0300

    Aula 255 e 256 - app todo list - criando a função createReducer

--- 4-redux/06-todo-list/src/redux-flow/reducers/todos/index.js
+++ 4-redux/06-todo-list/src/redux-flow/reducers/todos/index.js
@@ -1,30 +1,25 @@
 
 import {ADD_TODO, TOGGLE_TODO} from './actions';
+import createReducer from '../create-reducer';
 const initialState = [];
 
-const todos = (state = initialState, action) => {
-	switch (action.type) {
-		case ADD_TODO:
-			return state.concat({
-				id: action.payload.id,
-				text: action.payload.text,
-				completed: false
-			})
+const todos = createReducer(initialState, {
+	[ADD_TODO]: (state, action) => state.concat({
+		id: action.payload.id,
+		text: action.payload.text,
+		completed: false
+	}),
+	[TOGGLE_TODO]: (state, action) =>  state.map((todo) => {
+		if (todo.id !== action.payload.id) {
+			return todo
+		}
 
-		case TOGGLE_TODO:
-			return state.map((todo) => {
-				if (todo.id !== action.payload.id) {
-					return todo
-				}
+		return {
+			...todo,
+			completed: !todo.completed
+		}
+	})
 
-				return {
-					...todo,
-					completed: !todo.completed
-				}
-			})
-	}
-
-	return state
-}
+});
 
 export default todos
--- 4-redux/06-todo-list/src/redux-flow/reducers/visibility-filter/index.js
+++ 4-redux/06-todo-list/src/redux-flow/reducers/visibility-filter/index.js
@@ -1,14 +1,12 @@
 
+import createReducer from '../create-reducer';
 import {SET_VISIBILITY_FILTER, SHOW_ALL} from './actions';
 
 const initialState = SHOW_ALL;
 
-const visibilityFilter = (state = initialState, action) => {
-	switch (action.type) {
-		case SET_VISIBILITY_FILTER:
-			return action.payload.filter
-	}
-	return state
-}
+const visibilityFilter = createReducer(initialState, {
+	[SET_VISIBILITY_FILTER]: (state, action) => action.payload.filter
+});
+
 
 export default visibilityFilter
