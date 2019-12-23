6b8d21e85b320edaf21f1685cca4acab450c20fb
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-23 20:51:20 -0300

    Aula 271 - app todo list get cep - entendendo o poder dos middlewares

--- 4-redux/07-todo-list-get-cep/src/index.js
+++ 4-redux/07-todo-list-get-cep/src/index.js
@@ -8,13 +8,39 @@ import * as serviceWorker from './serviceWorker';
 import reducer from './redux-flow/reducers'
 
 const middleware = ({dispatch, getState}) => (next) => (action) => {
+	console.log('MIDDLEWARE::will dispatch: ', action);
 	const nextAction = next(action);
+	console.log('MIDDLEWARE::next action: ', nextAction);
 	return nextAction;
 };
 
+const thunk = ({dispatch, getState}) => (next) => (action) => {
+	if(typeof action === 'function'){
+		return action(dispatch, getState);
+	}
+	return next(action);
+};
 
+const store = createStore(reducer, applyMiddleware(middleware, thunk));
 
-const store = createStore(reducer, applyMiddleware(middleware));
+store.dispatch(lazyAction());
+function lazyAction(){
+	return (dispatch, getState) => {
+		dispatch({
+			type: 'todos:ADD_TODO',
+			payload:{
+				text: 'Lazy Action',
+				id: '123'
+			}
+		})
+	}
+}
+
+const renderState = () => {
+	console.log('state: ', store.getState());
+}
+store.subscribe(renderState)
+renderState()
 
 ReactDOM.render(
 	<Provider store={store}>
