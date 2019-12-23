89cac4cc1e44abfb3c0c5fba58d3b6dd365c1afd
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-23 20:34:37 -0300

    Aula 270 - app todo list get cep - extendendo o redux com middlewares

--- 4-redux/07-todo-list-get-cep/src/index.js
+++ 4-redux/07-todo-list-get-cep/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import reducer from './redux-flow/reducers'
 
-const store = createStore(reducer);
+const middleware = ({dispatch, getState}) => (next) => (action) => {
+	const nextAction = next(action);
+	return nextAction;
+};
+
+
+
+const store = createStore(reducer, applyMiddleware(middleware));
 
 ReactDOM.render(
 	<Provider store={store}>
