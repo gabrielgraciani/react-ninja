72f029bd5021a148b8b05ee16e75f17cfb0eae73
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-25 16:29:41 -0300

    Aula 284 - reactflix - lendo dados do database firebase

--- 4-redux/08-reactflix/src/components/video-single.js
+++ 4-redux/08-reactflix/src/components/video-single.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const VideoSingle = () => (
 	<div className="video-single">
-		<iframe width="560" height="480" src="https://www.youtube-nocookie.com/embed/8x4KROogMqo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<iframe width="560" height="480" src="https://www.youtube-nocookie.com/embed/8x4KROogMqo" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 		<h2 className="title">Título do vídeo</h2>
 	</div>
 );
