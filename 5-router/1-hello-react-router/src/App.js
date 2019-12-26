b42c5939569847b5ac3b2ba5d37912d046a5a4ab
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-26 20:10:44 -0300

    Aula 304 - hello react router dom - componentes principais do react router

--- 5-router/1-hello-react-router/src/App.js
+++ 5-router/1-hello-react-router/src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Link, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  	<BrowserRouter>
+		<div className="App">
+			<Link to="/sobre">Sobre</Link>
+
+			<Route path="/sobre" component={Sobre} />
+		</div>
+	</BrowserRouter>
   );
 }
+
+const Sobre = () => (
+	<h1>Sobre</h1>
+);
 
 export default App;
