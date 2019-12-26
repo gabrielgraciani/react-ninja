2fba33fd03e024cf5a0dd3488428fed4a5c33d37
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-26 20:17:36 -0300

    Aula 305 - hello react router dom - criando mais de uma rota

--- 5-router/1-hello-react-router/src/App.js
+++ 5-router/1-hello-react-router/src/App.js
@@ -5,16 +5,30 @@ function App() {
   return (
   	<BrowserRouter>
 		<div className="App">
-			<Link to="/sobre">Sobre</Link>
+			<ul>
+				<li><Link to="/">Home</Link></li>
+				<li><Link to="/sobre">Sobre</Link></li>
+				<li><Link to="/contato">Contato</Link></li>
+			</ul>
 
+			<Route path="/" exact component={Home} />
 			<Route path="/sobre" component={Sobre} />
+			<Route path="/contato" component={Contato} />
 		</div>
 	</BrowserRouter>
   );
 }
 
+const Home = () => (
+	<h1>Home</h1>
+);
+
 const Sobre = () => (
 	<h1>Sobre</h1>
 );
+
+const Contato = () => (
+	<h1>Contato</h1>
+)
 
 export default App;
