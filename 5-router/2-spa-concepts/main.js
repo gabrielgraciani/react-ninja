691b39cc30b6f21a8dfbf20b3ccb20ec9aaa6cca
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-27 11:35:30 -0300

    Aula 310 - spa-concepts - evento onpopstate

--- 5-router/2-spa-concepts/main.js
+++ 5-router/2-spa-concepts/main.js
@@ -1,17 +1,34 @@
-;(function(){
-	var $links = document.querySelectorAll('a');
-	var $h1 = document.querySelector('h1');
+;(function () {
 
-	$links.forEach(function ($link){
-		$link.addEventListener('click', handleClick, false);
-	});
+	var $links = document.querySelectorAll('a')
+	var $h1 = document.querySelector('h1')
 
-	function handleClick(e){
-		e.preventDefault();
-		var state = {};
-		var title = '';
-		var url = e.target.getAttribute('href');
-		history.pushState(state, title, url);
-		$h1.textContent = url;
+	$links.forEach(function ($link) {
+		$link.addEventListener('click', handleClick, false)
+	})
+
+	window.onpopstate = function (e) {
+		console.log('state:', e.state)
+		navigation({ url: e.state.url })
 	}
-})();
+
+	function navigation (state) {
+		var titles = {
+			'/': 'Home',
+			'/sobre': 'Sobre'
+		}
+
+		$h1.textContent = titles[state.url]
+	}
+
+	function handleClick (e) {
+		e.preventDefault()
+		var title = ''
+		var url = e.target.getAttribute('href')
+		var state = {
+			url: url
+		}
+		history.pushState(state, title, url)
+		navigation({ url: url })
+	}
+})()
