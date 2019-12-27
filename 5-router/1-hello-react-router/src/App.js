b24e3e5fafa0ab3da6de7f262fcfa07ff5bc96eb
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-27 19:39:03 -0300

    Aula 316 - hello react router dom - NavLink - prop "render"

--- 5-router/1-hello-react-router/src/App.js
+++ 5-router/1-hello-react-router/src/App.js
@@ -3,39 +3,46 @@ import {BrowserRouter, Route, Switch, NavLink} from 'react-router-dom';
 import './assets/css/styles.css';
 
 const Link = (props) => (
-	<NavLink activeStyle={{color: 'red'}} {...props} />
+	<NavLink activeStyle={{ color: 'red' }} {...props} />
 );
 
-class App extends Component{
-	render(){
-	  return (
-		<BrowserRouter>
-			<div className="App">
-				<ul>
-					<li><Link to="/" exact>Home</Link></li>
-					<li><Link to="/sobre">Sobre</Link></li>
-					<li><Link to="/blog">Blog</Link></li>
-					<li><Link to="/contato">Contato</Link></li>
-				</ul>
+class App extends Component {
+	render () {
+		return (
+			<BrowserRouter>
+				<div>
+					<ul>
+						<li><Link to='/' exact>Home</Link></li>
+						<li><Link to='/sobre'>Sobre</Link></li>
+						<li><Link to='/contato'>Contato</Link></li>
+						<li><Link to='/blog'>Blog</Link></li>
+					</ul>
 
-				<Switch>
-					<Route path="/" exact component={Home} />
-					<Route path="/(sobre|contato)" component={Page} />
-					<Route path="/blog" component={Blog} />
-					<Route component={Error404} />
-				</Switch>
-			</div>
-		</BrowserRouter>
-	  );
+					<Switch>
+						<Route path='/' exact component={Home} />
+						<Route path='/(sobre|contato)' component={Page} />
+						<Route path='/blog' component={Blog} />
+						<Route component={Error404} />
+					</Switch>
+				</div>
+			</BrowserRouter>
+		)
 	}
 }
+
+const Error404 = () => (
+	<h1>Página não encontrada</h1>
+);
 
 const Home = () => (
 	<h1>Home</h1>
 );
 
-const Page = ({match}) => (
-	<h1>{match.url}</h1>
+const Page = ({ match }) => (
+	<div>
+		{console.log(match)}
+		<h1>{match.url}</h1>
+	</div>
 );
 
 const Blog = () => (
@@ -43,23 +50,30 @@ const Blog = () => (
 		<h1>Blog</h1>
 
 		<ul>
-			<li><Link to="/blog/post-1">Post1</Link></li>
-			<li><Link to="/blog/post-2">Post2</Link></li>
+			<li><Link to='/blog/post-1'>Post 1</Link></li>
+			<li><Link to='/blog/post-2'>Post 2</Link></li>
 		</ul>
 
-		<Route path="/blog/:post(post-1|post-2)" component={Post} />
+		<Switch>
+			<Route exact path='/blog' render={(props) => <NoPosts numberOfPosts={2} {...props} />} />
+			<Route path='/blog/:post(post-[12])' component={Post} />
+			<Route component={Post404} />
+		</Switch>
 	</div>
 );
 
-const Post = ({match}) => (
+const Post404 = () => (
+	<h1>Esse post não existe</h1>
+);
+
+const Post = ({ match }) => (
 	<div>
 		<h2>Post: {match.params.post}</h2>
 	</div>
 );
 
-const Error404 = () => (
-	<h1>Página não encontrada</h1>
+const NoPosts = ({ numberOfPosts }) => (
+	<p>Selecione um dos {numberOfPosts} posts</p>
 );
 
-
-export default App;
+export default App
