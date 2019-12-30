3a436c6eb8c0685250d376a174ab15f467311577
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-30 08:35:55 -0300

    Aula 340 - reactzzaria - mensagens de erros (error boundaries

--- 6-reactzzaria/sistema-pedidos/src/error.js
+++ 6-reactzzaria/sistema-pedidos/src/error.js
@@ -2,9 +2,16 @@ import {Component} from 'react';
 
 class ErrorBoundary extends Component{
 	state = {hasError: false};
-	static getDerivedStateFromError(){
+	static getDerivedStateFromError(error){
+		console.log('error getDerivedStateFromError:', error);
 		return {hasError: true}
 	}
+
+	componentDidCatch(error, info){
+		console.log('error:', error);
+		console.log('info:', info);
+	}
+
 	render(){
 		return this.props.children(this.state.hasError)
 	}
