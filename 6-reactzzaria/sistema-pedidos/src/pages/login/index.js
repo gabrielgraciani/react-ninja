881cc384bc5ac80676eb3a1d956805f4405bbd24
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-30 16:48:50 -0300

    Aula 356 - reactzzaria - configurar firebase no app

--- 6-reactzzaria/sistema-pedidos/src/pages/login/index.js
+++ 6-reactzzaria/sistema-pedidos/src/pages/login/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import {Button, Grid} from '@material-ui/core';
 import {ReactComponent as Logo} from './logo-react-zzaria.svg';
+import firebase from 'firebase/app';
+
+var firebaseConfig = {
+apiKey: "AIzaSyBFLmUSiM1Iq9mFGdORfhIPgPWlmwIA2JE",
+authDomain: "reactzzaria-a7216.firebaseapp.com",
+databaseURL: "https://reactzzaria-a7216.firebaseio.com",
+projectId: "reactzzaria-a7216",
+storageBucket: "reactzzaria-a7216.appspot.com",
+messagingSenderId: "1004184943272",
+appId: "1:1004184943272:web:a1251665128a05be9f209d",
+measurementId: "G-XDP1NGPZFX"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const Login = () => (
 	<div id="wrap_login">
