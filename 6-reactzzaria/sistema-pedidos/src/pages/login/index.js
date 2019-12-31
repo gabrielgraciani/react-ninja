49adc8d23288071ad7383edd9faa34744b25e391
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-31 13:34:41 -0300

    Aula 379 - reactzzaria - dados do usuario na Context API (app)

--- 6-reactzzaria/sistema-pedidos/src/pages/login/index.js
+++ 6-reactzzaria/sistema-pedidos/src/pages/login/index.js
@@ -1,39 +1,11 @@
-import React, {useState, useEffect, useCallback, useContext} from 'react';
+import React, {useContext} from 'react';
 import {Button, Grid} from '@material-ui/core';
 import {ReactComponent as Logo} from './logo-react-zzaria.svg';
 import {AuthContext} from 'contexts/auth';
-import firebase from 'services/firebase';
 
 
 function Login(){
-	const [userInfo, setUserInfo] = useState({
-		isUserLoggedIn: false,
-		user: null
-	});
 	const {login} = useContext(AuthContext);
-
-	const {isUserLoggedIn, user} = userInfo;
-
-	useEffect(() => {
-		firebase.auth().onAuthStateChanged((user) => {
-			console.log('dados do usuario:', user);
-			setUserInfo({
-				isUserLoggedIn: !!user,
-				user
-			});
-		})
-	}, []);
-
-
-	const logout = useCallback(() =>{
-		firebase.auth().signOut().then(() => {
-			console.log('deslogou');
-			setUserInfo({
-				isUserLoggedIn: false,
-				user: null
-			});
-		})
-	}, []);
 
 	return(
 		<div id="wrap_login">
@@ -43,17 +15,8 @@ function Login(){
 				</Grid>
 
 				<Grid item xs={12} container justify="center">
-					{isUserLoggedIn && (
-						<>
-						<pre>{user.email}</pre>
-						<Button variant="contained" onClick={logout}>Sair</Button>
-						</>
-					)}
-
-					{!isUserLoggedIn && (
-						<Button variant="contained" fullWidth className="githubbutton"
-								onClick={login}>Entrar com GitHub</Button>
-					)}
+					<Button variant="contained" fullWidth className="githubbutton"
+							onClick={login}>Entrar com GitHub</Button>
 				</Grid>
 			</Grid>
 		</div>
