93f5d3ed06fb4fd54081286f8b0753db003eb74f
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-31 14:02:57 -0300

    Aula 382 - reactzzaria - refatorar regras de redirects

--- 6-reactzzaria/sistema-pedidos/src/app.js
+++ 6-reactzzaria/sistema-pedidos/src/app.js
@@ -27,34 +27,17 @@ function App ({ location }) {
 	}, []);
 
 	if(!didCheckUserIn){
-		console.log('ainda não checou se usuário está logado ou não.');
 		return <LinearProgress />
 	}
-	console.log('já checou se o usuário está logado ou não');
 
-	if (isUserLoggedIn) {
-		console.log('usuário está logado');
-		if (location.pathname === '/login') {
-			console.log(
-				`usuário está logado E está na página de login.
-        Redirecionar para home /`
-			);
-			return <Redirect to='/' />
-		} else {
-			console.log('usuário está logado MAS NÃO está na página de login.');
-		}
-	} else {
-		console.log('usuário não está logado');
-		if (location.pathname !== '/login') {
-			console.log(`
-        usuário não está logado, NEM está na página de login.
-        Redirecionar para /login
-      `);
-			return <Redirect to='/login' />
-		} else {
-			console.log('usuário não está logado E está na página de login.');
-		}
+	if(isUserLoggedIn && location.pathname === "/login"){
+		return <Redirect to="/" />
 	}
+
+	if(!isUserLoggedIn && location.pathname !== "/login"){
+		return <Redirect to="/login" />
+	}
+
 
 	return (
 		<Suspense fallback={<LinearProgress />}>
