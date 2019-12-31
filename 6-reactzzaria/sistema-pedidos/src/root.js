2142afe6b62330975fbfc6708eed846a5e96c5d9
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-31 17:06:32 -0300

    Aula 390 - reactzzaria - como usar temas no material ui

--- 6-reactzzaria/sistema-pedidos/src/root.js
+++ 6-reactzzaria/sistema-pedidos/src/root.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
-import {CssBaseline} from '@material-ui/core';
+import {CssBaseline, createMuiTheme, MuiThemeProvider} from '@material-ui/core';
 import 'assets/css/styles.css';
 import App from './app';
 import AuthProvider from 'contexts/auth';
 
+const theme = createMuiTheme({
+	typography: {
+		useNextVariants: true
+	}
+});
 
 const Root = () =>(
-	<AuthProvider>
-		<CssBaseline />
+	<MuiThemeProvider theme={theme}>
+		<AuthProvider>
+			<CssBaseline />
 
-		<BrowserRouter>
-			<Route component={App} />
-		</BrowserRouter>
-	</AuthProvider>
+			<BrowserRouter>
+				<Route component={App} />
+			</BrowserRouter>
+		</AuthProvider>
+	</MuiThemeProvider>
+
 );
 
 export default Root;
