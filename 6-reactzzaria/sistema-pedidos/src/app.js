c98f2cddbb509f0705b1cdeaeb069155331b8eeb
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-31 13:50:44 -0300

    Aula 380 - reactzzaria - regras de redirects pré/pos autenticação

--- 6-reactzzaria/sistema-pedidos/src/app.js
+++ 6-reactzzaria/sistema-pedidos/src/app.js
@@ -1,33 +1,63 @@
-import React, {Suspense, lazy, useEffect, useContext} from 'react';
-import {Route, Switch} from 'react-router-dom';
-import {LinearProgress} from '@material-ui/core';
+import React, { lazy, useEffect, useContext, Suspense } from 'react';
+import t from 'prop-types';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { LinearProgress } from '@material-ui/core';
 import firebase from 'services/firebase';
-import {AuthContext} from 'contexts/auth';
+import { AuthContext } from 'contexts/auth';
 
 const MainPage = lazy(() => import('pages/main'));
 const Login = lazy(() => import('pages/login'));
 
-function App(){
-	const {setUserInfo} = useContext(AuthContext);
+function App ({ location }) {
+	const { userInfo, setUserInfo } = useContext(AuthContext);
+	const { isUserLoggedIn } = userInfo;
 
 	useEffect(() => {
 		firebase.auth().onAuthStateChanged((user) => {
-			console.log('dados do usuario:', user);
+			console.log('dados do usuário:', user);
 			setUserInfo({
 				isUserLoggedIn: !!user,
-				user,
-			});
+				user
+			})
 		})
 	}, []);
 
-	return(
+	if (isUserLoggedIn) {
+		console.log('usuário está logado');
+		if (location.pathname === '/login') {
+			console.log(
+				`usuário está logado E está na página de login.
+        Redirecionar para home /`
+			);
+			return <Redirect to='/' />
+		} else {
+			console.log('usuário está logado MAS NÃO está na página de login.');
+		}
+	} else {
+		console.log('usuário não está logado');
+		if (location.pathname !== '/login') {
+			console.log(`
+        usuário não está logado, NEM está na página de login.
+        Redirecionar para /login
+      `);
+			return <Redirect to='/login' />
+		} else {
+			console.log('usuário não está logado E está na página de login.');
+		}
+	}
+
+	return (
 		<Suspense fallback={<LinearProgress />}>
 			<Switch>
-				<Route path="/login" component={Login} />
-				<Route path="/" component={MainPage} />
+				<Route path='/login' component={Login} />
+				<Route component={MainPage} />
 			</Switch>
 		</Suspense>
 	)
 }
 
-export default App;
+App.propTypes = {
+	location: t.object.isRequired
+};
+
+export default App
