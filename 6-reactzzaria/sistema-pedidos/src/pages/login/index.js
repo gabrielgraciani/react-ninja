274f9049d51c87c9162509ba7c643cc5be23fe4b
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-31 09:11:54 -0300

    Aula 371 - reactzzaria - trocar classe por hooks na página de login

--- 6-reactzzaria/sistema-pedidos/src/pages/login/index.js
+++ 6-reactzzaria/sistema-pedidos/src/pages/login/index.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Button, Grid} from '@material-ui/core';
 import {ReactComponent as Logo} from './logo-react-zzaria.svg';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-var firebaseConfig = {
+const firebaseConfig = {
 apiKey: "AIzaSyBFLmUSiM1Iq9mFGdORfhIPgPWlmwIA2JE",
 authDomain: "reactzzaria-a7216.firebaseapp.com",
 databaseURL: "https://reactzzaria-a7216.firebaseio.com",
@@ -17,61 +17,62 @@ measurementId: "G-XDP1NGPZFX"
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-class Login extends Component {
-	state = {
+const login = () =>{
+	const provider = new firebase.auth.GithubAuthProvider();
+	firebase.auth().signInWithRedirect(provider);
+};
+
+function Login(){
+	const [userInfo, setUserInfo] = useState({
 		isUserLoggedIn: false,
 		user: null
-	};
+	});
 
-	componentDidMount(){
+	const {isUserLoggedIn, user} = userInfo;
+
+	useEffect(() => {
 		firebase.auth().onAuthStateChanged((user) => {
 			console.log('dados do usuario:', user);
-			this.setState({
+			setUserInfo({
 				isUserLoggedIn: !!user,
 				user
 			});
 		})
-	}
+	}, [])
 
-	login(){
-		const provider = new firebase.auth.GithubAuthProvider();
-		firebase.auth().signInWithRedirect(provider);
-	}
-
-	logout = () =>{
+	const logout = () =>{
 		firebase.auth().signOut().then(() => {
 			console.log('deslogou');
-			this.setState({isUserLoggedIn: false, user: null});
+			setUserInfo({
+				isUserLoggedIn: false,
+				user: null
+			});
 		})
 	};
 
-	render(){
-		const {isUserLoggedIn, user} = this.state;
-		return(
-			<div id="wrap_login">
-				<Grid container justify='center' spacing={7}>
-					<Grid item>
-						<Logo />
-					</Grid>
-
-					<Grid item xs={12} container justify="center">
-						{isUserLoggedIn && (
-							<>
-								<pre>{user.email}</pre>
-							<Button variant="contained" onClick={this.logout}>Sair</Button>
-							</>
-						)}
-
-						{!isUserLoggedIn && (
-							<Button variant="contained" fullWidth className="githubbutton"
-									onClick={this.login}>Entrar com GitHub</Button>
-						)}
-
-					</Grid>
+	return(
+		<div id="wrap_login">
+			<Grid container justify='center' spacing={7}>
+				<Grid item>
+					<Logo />
 				</Grid>
-			</div>
-		);
-	}
-};
+
+				<Grid item xs={12} container justify="center">
+					{isUserLoggedIn && (
+						<>
+						<pre>{user.email}</pre>
+						<Button variant="contained" onClick={logout}>Sair</Button>
+						</>
+					)}
+
+					{!isUserLoggedIn && (
+						<Button variant="contained" fullWidth className="githubbutton"
+								onClick={login}>Entrar com GitHub</Button>
+					)}
+				</Grid>
+			</Grid>
+		</div>
+	);
+}
 
 export default Login;
