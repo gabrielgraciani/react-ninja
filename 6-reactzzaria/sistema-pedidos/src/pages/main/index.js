5db7ff4bc535675e5fc9f27c13d4338f607fbd75
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2019-12-31 14:15:45 -0300

    Aula 384 - reactzzaria - página inicial (parte 2 - header)

--- 6-reactzzaria/sistema-pedidos/src/pages/main/index.js
+++ 6-reactzzaria/sistema-pedidos/src/pages/main/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {AppBar, Toolbar, IconButton} from '@material-ui/core';
+import {AppBar, Toolbar, IconButton, Typography, Menu, MenuItem} from '@material-ui/core';
 import {AccountCircle} from '@material-ui/icons';
 import {ReactComponent as Logo} from 'assets/images/logo-react-zzaria.svg';
 
@@ -8,9 +8,16 @@ const Main = () => (
 	<AppBar>
 		<Toolbar>
 			<Logo />
+
+			<Typography color="inherit">Olá João =)</Typography>
+
 			<IconButton color="inherit">
 				<AccountCircle />
 			</IconButton>
+
+			<Menu open>
+				<MenuItem>Sair</MenuItem>
+			</Menu>
 		</Toolbar>
 	</AppBar>
 );
