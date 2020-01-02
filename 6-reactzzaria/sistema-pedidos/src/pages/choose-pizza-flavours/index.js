1521264fa581eb3cdd437c2ba6c572e390e2119c
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-01-02 10:05:07 -0300

    Aula 408 - reactzzaria - regras para seleção de sabores

--- 6-reactzzaria/sistema-pedidos/src/pages/choose-pizza-flavours/index.js
+++ 6-reactzzaria/sistema-pedidos/src/pages/choose-pizza-flavours/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import HeaderContent from 'ui/header-content';
 import SingularOrPlural from 'utils';
 import {Redirect} from 'react-router-dom';
@@ -9,10 +9,21 @@ import {Link} from 'react-router-dom';
 
 
 const ChoosePizzaFlavours = ({location}) => {
+	const [checkboxes, setCheckboxes] = useState(() => ({}));
+	console.log('checkboxes', checkboxes);
 	if(!location.state){
 		return <Redirect to={HOME} />
 	}
 	const {flavours, id} = location.state;
+	const handleChangeCheckbox = (pizzaId) => (e) => {
+		setCheckboxes((checkboxes) => {
+			return{
+				...checkboxes,
+				[pizzaId]: !checkboxes[pizzaId]
+			}
+		});
+	};
+
 	return(
 		<>
 			<HeaderContent>
@@ -21,13 +32,15 @@ const ChoosePizzaFlavours = ({location}) => {
 				</div>
 			</HeaderContent>
 
-
 			<Grid container spacing={3} className="container-pizza">
 				{pizzasFlavours.map((pizza) => (
 					<Grid item key={pizza.id} xs>
 						<Paper className="full">
 							<label>
-								<input type="checkbox" />
+								<input type="checkbox"
+									   value=""
+									   checked={!!checkboxes[pizza.id]}
+									   onChange={handleChangeCheckbox(pizza.id)} />
 								<div className="pizza-imagem icon">
 									<img src={pizza.image} alt={pizza.name} />
 								</div>
