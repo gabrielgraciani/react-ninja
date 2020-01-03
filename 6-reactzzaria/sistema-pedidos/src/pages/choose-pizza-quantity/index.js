b18c76782a4e88e23dca2a4c4327a416aad41305
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-01-03 10:51:50 -0300

    Aula 422 - reactzzaria - marcação inicial da página de escolha de quantidade

--- 6-reactzzaria/sistema-pedidos/src/pages/choose-pizza-quantity/index.js
+++ 6-reactzzaria/sistema-pedidos/src/pages/choose-pizza-quantity/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import HeaderContent from 'ui/header-content'
 
 function ChoosePizzaQuantity() {
 	return(
-		<h1>asd</h1>
+		<>
+			<HeaderContent>
+				<div className="texto top">
+					<h4>Quantas pizzas você gostaria <br />
+						de pedir, com esses sabores?</h4>
+				</div>
+			</HeaderContent>
+
+			<div id="wrap_quantidade">
+				<div className="indent">
+					<div className="input">
+						<input type="number" value="1" autoFocus />
+					</div>
+				</div>
+			</div>
+
+
+		</>
 	)
 }
 
