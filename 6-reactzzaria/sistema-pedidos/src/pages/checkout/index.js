f8a32546fc0495ac76c8bb8ad0ff8edb08e21b53
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-01-03 19:46:27 -0300

    Aula 437 - reactzzaria - marcação da página de checkout

--- 6-reactzzaria/sistema-pedidos/src/pages/checkout/index.js
+++ 6-reactzzaria/sistema-pedidos/src/pages/checkout/index.js
@@ -2,7 +2,43 @@ import React from 'react';
 
 function Checkout(){
 	return(
-		<h1>checkout</h1>
+		<div id="wrap_checkout">
+			<div className="indent">
+				<div className="col1">
+					<div className="bloco">
+						<div className="titulo">
+							<h5>Qual o endereço para entrega?</h5>
+						</div>
+
+						<div className="conteudo">
+							qweewqewq
+						</div>
+					</div>
+
+					<div className="bloco top">
+						<div className="titulo">
+							<h5>Qual seu telefone</h5>
+						</div>
+
+						<div className="conteudo">
+							qweewqewq
+						</div>
+					</div>
+				</div>
+
+				<div className="col2">
+					<div className="bloco">
+						<div className="titulo">
+							<h5>Informações do seu pedido:</h5>
+						</div>
+
+						<div className="conteudo">
+							qweewqewq
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	)
 }
 
