f113c04352a967022b3361582e7cae3ac3c37e1d
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-01-04 15:59:16 -0300

    Aula 444 - reactzzaria - header para página de confirmação de pedido

--- 6-reactzzaria/sistema-pedidos/src/pages/checkout-confirmation/index.js
+++ 6-reactzzaria/sistema-pedidos/src/pages/checkout-confirmation/index.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import {useAuth} from 'hooks';
 
 function CheckoutConfirmation(){
+	const {userInfo} = useAuth();
 	return(
-		<h1>checkout confirmation</h1>
+		<div id="wrap_checkout" className="confirmation">
+			<div className="indent">
+				<div className="texto">
+					<h4>Oi {userInfo.user.firstName}</h4>
+					<span>Confere, por favor, se está tudo certo com o seu pedido antes de finalizar?</span>
+				</div>
+
+				<div className="bloco">
+					<div className="titulo">
+						<h5>Seu Pedido</h5>
+					</div>
+					<div className="texto">
+						<span>asdhasodi</span>
+					</div>
+
+					<div className="titulo top">
+						<h5>Endereço para entrega:</h5>
+					</div>
+					<div className="texto">
+						<span>asdhasodi</span>
+					</div>
+
+					<div className="titulo top">
+						<h5>Telefone para contato:</h5>
+					</div>
+					<div className="texto">
+						<span>asdhasodi</span>
+					</div>
+				</div>
+			</div>
+		</div>
 	)
 }
 
