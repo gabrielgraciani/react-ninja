09f6338aa434c41e850b83f634fcc56b613c81a4
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-01-04 16:25:42 -0300

    Aula 447 - reactzzaria - rodape para pagina de confirmação de pedido

--- 6-reactzzaria/sistema-pedidos/src/pages/checkout-confirmation/index.js
+++ 6-reactzzaria/sistema-pedidos/src/pages/checkout-confirmation/index.js
@@ -1,34 +1,47 @@
 import React from 'react';
 import {useAuth} from 'hooks';
 import OrderInfo from 'ui/order-info';
+import Footer from 'ui/footer';
+import {Link} from 'react-router-dom';
 
 function CheckoutConfirmation(){
 	const {userInfo} = useAuth();
 	return(
-		<div id="wrap_checkout" className="confirmation">
-			<div className="indent">
-				<div className="texto">
-					<h4>Oi {userInfo.user.firstName}</h4>
-					<span>Confere, por favor, se está tudo certo com o seu pedido antes de finalizar?</span>
-				</div>
-
-				<div className="conteudo">
-					<div className="titulo">
-						<h5>Seu Pedido:</h5>
-					</div>
-					<OrderInfo />
-
-					<div className="titulo top">
-						<h5>Endereço para entrega:</h5>
+		<>
+			<div id="wrap_checkout" className="confirmation">
+				<div className="indent">
+					<div className="texto">
+						<h4>Oi {userInfo.user.firstName}</h4>
+						<span>Confere, por favor, se está tudo certo com o seu pedido antes de finalizar?</span>
 					</div>
 
+					<div className="conteudo">
+						<div className="titulo">
+							<h5>Seu Pedido:</h5>
+						</div>
+						<OrderInfo />
 
-					<div className="titulo top">
-						<h5>Telefone para contato:</h5>
+						<div className="titulo top">
+							<h5>Endereço para entrega:</h5>
+						</div>
+
+
+						<div className="titulo top">
+							<h5>Telefone para contato:</h5>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+
+			<Footer>
+				<div className="texto centro">
+					<Link>
+						<button className="botao ripple azul">TUDO CERTO!</button>
+					</Link>
+				</div>
+			</Footer>
+		</>
+
 	)
 }
 
