005a14a8525343a8fa9977f860a871e56168894d
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-01-05 08:58:25 -0300

    Aula 455 - reactzzaria - estado para campo de CEP com máscara

--- 6-reactzzaria/sistema-pedidos/src/pages/checkout/form-address.js
+++ 6-reactzzaria/sistema-pedidos/src/pages/checkout/form-address.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function FormAddress(){
+	const [cep, setCep] = useState('');
+
+	function handleChangeCep(e){
+		setCep(cepMask(e.target.value));
+	}
+
+	function cepMask(value){
+		return value
+			.replace(/\D+/g, '')
+			.replace(/(\d{5})(\d)/, '$1-$2')
+			.replace(/(-\d{3})\d+?$/, '$1');
+	}
 	return(
 		<>
 		<div className="conteudo">
 			<div className="item">
 				<div className="input">
 					<label>CEP</label>
-					<input type="text" autoFocus />
+					<input type="text" autoFocus value={cep} onChange={handleChangeCep} />
 				</div>
 				<div className="input maior">
 					<label>Rua</label>
