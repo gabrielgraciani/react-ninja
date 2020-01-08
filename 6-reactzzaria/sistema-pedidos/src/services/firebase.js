d467662ae0a1a19e20a53867c6b709033f5158df
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2020-01-07 21:47:25 -0300

    Aula 470 - reactzzaria - variaveis de ambiente para salvar chaves de segurança

--- 6-reactzzaria/sistema-pedidos/src/services/firebase.js
+++ 6-reactzzaria/sistema-pedidos/src/services/firebase.js
@@ -3,14 +3,15 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-	apiKey: "AIzaSyBFLmUSiM1Iq9mFGdORfhIPgPWlmwIA2JE",
-	authDomain: "reactzzaria-a7216.firebaseapp.com",
-	databaseURL: "https://reactzzaria-a7216.firebaseio.com",
-	projectId: "reactzzaria-a7216",
-	storageBucket: "reactzzaria-a7216.appspot.com",
-	messagingSenderId: "1004184943272",
-	appId: "1:1004184943272:web:a1251665128a05be9f209d",
-	measurementId: "G-XDP1NGPZFX"
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_DATABASE_URL,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
+	measurementId: process.env.REACT_APP_MEASUREMENT_ID
+
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
